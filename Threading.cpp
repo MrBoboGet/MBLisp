0104ef2bd3b752626c56a302a8456ce40e850217
MrBoboGet <emanuelberggren01@gmail.com>
2023-09-28 03:39:22 +0200

    And threading...

---
+++ Threading.cpp
@@ -0,0 +1,291 @@
+#include "Threading.h"
+
+
+namespace MBLisp
+{
+       
+    void ThreadingState::p_ScheduleNext()
+    {
+        clock_t LastClock = m_LastClock.load();
+        clock_t CurrentClock = clock();
+        double Diff = (CurrentClock-LastClock)/double(CLOCKS_PER_SEC);
+        m_ElapsedTime.store(m_ElapsedTime.load() + Diff);
+        if(m_ElapsedTime.load() > m_SwapTime)
+        {
+            ThreadID NextID = -1;
+            float MinSleepTime = 1000;
+            for(auto& ThreadIt : m_ActiveThreads)
+            {
+                if(ThreadIt.second->SleepDuration > 0)
+                {
+                    ThreadIt.second->SleepDuration -= Diff;   
+                    if(ThreadIt.second->SleepDuration < 0)
+                    {
+                        ThreadIt.second->SleepDuration = 0;
+                    }
+                }
+                if(!ThreadIt.second->Paused && ThreadIt.second->SleepDuration <= 0 && ThreadIt.first != m_CurrentThread)
+                {
+                    NextID = ThreadIt.first;
+                }
+                if(!ThreadIt.second->Paused)
+                {
+                    MinSleepTime = std::min(MinSleepTime,ThreadIt.second->SleepDuration);
+                }
+            }
+            if(NextID == -1 && m_ActiveThreads[m_CurrentThread]->SleepDuration > 0)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(int(MinSleepTime*1000)));
+                p_ScheduleNext();
+                return;
+            }
+            if(NextID != -1)
+            {
+                auto ThreadIt = m_ActiveThreads.find(NextID);
+                assert(ThreadIt != m_ActiveThreads.end());
+                std::shared_ptr<ThreadSchedulingInfo>& ThreadToWake = ThreadIt->second;
+                std::lock_guard<std::mutex> WakeMutex(ThreadToWake->WaitMutex);
+                ThreadToWake->WakedUp = true;
+                ThreadToWake->WaitConditional.notify_one();
+                m_CurrentThread = NextID;
+            }
+            //new thread to schedule
+        }
+        m_LastClock.store(clock());
+    }
+    void ThreadingState::p_StartNext()
+    {
+        clock_t LastClock = m_LastClock.load();
+        clock_t CurrentClock = clock();
+        double Diff = (CurrentClock-LastClock)/double(CLOCKS_PER_SEC);
+        m_ElapsedTime.store(m_ElapsedTime.load() + Diff);
+        if(m_ElapsedTime.load() > m_SwapTime)
+        {
+            ThreadID NextID = -1;
+            float LowestSleepTime = std::numeric_limits<float>::max();
+            float MinSleepTime = 1000;
+            for(auto& ThreadIt : m_ActiveThreads)
+            {
+                if(ThreadIt.second->SleepDuration > 0)
+                {
+                    ThreadIt.second->SleepDuration -= Diff;   
+                    if(ThreadIt.second->SleepDuration < 0)
+                    {
+                        ThreadIt.second->SleepDuration = 0;
+                    }
+                }
+                if(ThreadIt.second->Paused || ThreadIt.first == m_CurrentThread)
+                {
+                    continue;
+                }
+                if(!ThreadIt.second->Paused && ThreadIt.second->SleepDuration <= 0 && ThreadIt.first != m_CurrentThread)
+                {
+                    if(ThreadIt.second->SleepDuration < LowestSleepTime)
+                    {
+                        NextID = ThreadIt.first;
+                        LowestSleepTime = ThreadIt.second->SleepDuration;
+                    }
+                }
+            }
+            if(NextID != -1)
+            {
+                std::shared_ptr<ThreadSchedulingInfo> ThreadToWake = m_ActiveThreads[NextID];
+                std::lock_guard<std::mutex> WakeMutex(ThreadToWake->WaitMutex);
+                ThreadToWake->WakedUp = true;
+                ThreadToWake->WaitConditional.notify_one();
+                ThreadToWake->SleepDuration = 0;
+                m_CurrentThread = NextID;
+            }
+            //new thread to schedule
+        }
+        m_LastClock.store(clock());
+           
+    }
+    ThreadingState::ThreadingState()
+    {
+        auto& NewThread = m_ActiveThreads[0];
+        NewThread = std::make_unique<ThreadSchedulingInfo>();
+    }
+    ThreadingState::~ThreadingState()
+    {
+        m_Exiting.store(true);
+        for(auto& ThreadIt : m_ActiveThreads)
+        {
+            {
+                std::lock_guard<std::mutex> WakeMutex(ThreadIt.second->WaitMutex);
+                ThreadIt.second->WaitConditional.notify_one();
+                ThreadIt.second->WakedUp = true;
+            }
+            if(ThreadIt.second->SystemThread != nullptr)
+            {
+                ThreadIt.second->SystemThread->join();
+            }
+        }
+    }
+
+    bool ThreadingState::MultipleThreadsActive()
+    {
+        return m_ThreadCount.load() > 0; 
+    }
+    void ThreadingState::WaitForTurn(ThreadID ID)
+    {
+        std::shared_ptr<ThreadSchedulingInfo> ThreadInfo;
+        m_ThreadInfoMutex.lock();
+        bool Unlocked = false;
+        auto ThreadIt = m_ActiveThreads.find(ID);
+        if(ThreadIt == m_ActiveThreads.end())
+        {
+            m_ThreadInfoMutex.unlock();
+            throw std::runtime_error("Invalid thread ID when calling WaitForTurn");   
+        }
+        ThreadInfo = ThreadIt->second;
+        p_ScheduleNext();
+        if(m_CurrentThread != ID)
+        {
+            std::unique_lock<std::mutex> Lock(ThreadInfo->WaitMutex);
+            Unlocked = true;
+
+#if !defined(NDEBUG)
+            bool AllArePaused = true;
+            for(auto const& Info : m_ActiveThreads)
+            {
+                if(Info.first != ID && Info.second->Paused == false)
+                {
+                    AllArePaused = false;
+                    break;
+                }
+            }
+            assert(!AllArePaused);
+#endif
+            assert(ThreadInfo->SleepDuration <= 0);
+            m_ThreadInfoMutex.unlock();
+            while(!ThreadInfo->WakedUp)
+            {
+                ThreadInfo->WaitConditional.wait(Lock);
+            }
+            ThreadInfo->WakedUp = false;
+        }
+        if(!Unlocked)
+        {
+            m_ThreadInfoMutex.unlock();   
+        }
+        if(m_Exiting.load())
+        {
+            throw std::runtime_error("Should exit");   
+        }
+    }
+
+    void ThreadingState::Pause(ThreadID ID)
+    {
+        std::lock_guard<std::mutex> Lock(m_ThreadInfoMutex);
+        auto It = m_ActiveThreads.find(ID);
+        if(It == m_ActiveThreads.end())
+        {
+            throw std::runtime_error("Invalid thread id: no active thread with id "+std::to_string(ID));
+        }
+        if(It->second != nullptr)
+        {
+            std::lock_guard<std::mutex> ThreadLock(It->second->WaitMutex);
+            It->second->Paused = true;
+        }
+        //in order for lock's in builtin functions to work so must this function also take care of the case where
+        //ID == m_CurrentThread and schedule a new one.
+        if(ID == m_CurrentThread)
+        {
+            p_StartNext();
+        }
+    }
+    void ThreadingState::Resume(ThreadID ID)
+    {
+        std::lock_guard<std::mutex> Lock(m_ThreadInfoMutex);
+        auto It = m_ActiveThreads.find(ID);
+        if(It == m_ActiveThreads.end())
+        {
+            throw std::runtime_error("Invalid thread id: no active thread with id "+std::to_string(ID));
+        }
+        if(It->second != nullptr)
+        {
+            std::lock_guard<std::mutex> ThreadLock(It->second->WaitMutex);
+            It->second->Paused = false;
+        }
+    }
+    void ThreadingState::Remove(ThreadID ID)
+    {
+        if(m_Exiting) 
+        {
+            return;
+        }
+        std::shared_ptr<ThreadSchedulingInfo> InfoToRemove;
+        {
+            std::lock_guard<std::mutex> InternalsLock(m_ThreadInfoMutex);
+            auto ThreadIt = m_ActiveThreads.find(ID);
+            if(ThreadIt == m_ActiveThreads.end())
+            {
+                throw std::runtime_error("Invalid thread ID when calling remove");   
+            }
+            InfoToRemove = std::move(ThreadIt->second);
+            m_ActiveThreads.erase(ThreadIt);
+        }
+        m_ThreadCount.fetch_sub(1);
+        if(InfoToRemove->Paused)
+        {
+            InfoToRemove->WaitConditional.notify_one();
+        }
+    }
+    ThreadID ThreadingState::AddThread(std::function<void()> Func)
+    {
+        std::lock_guard<std::mutex> InternalsLock(m_ThreadInfoMutex);
+        m_ThreadCount.fetch_add(1);
+        ThreadID ReturnValue = m_CurrentID;
+        m_CurrentID++;
+        auto& NewThread = m_ActiveThreads[ReturnValue];
+        NewThread = std::make_unique<ThreadSchedulingInfo>();
+        NewThread->SystemThread = std::make_unique<std::thread>(
+                    [&,Func=std::move(Func)](ThreadID ID)
+                    {
+                        try
+                        {
+                            Func();
+                        }
+                        catch(...)
+                        {
+                            
+                        }
+                        try
+                        {
+                            this->Remove(ID);
+                        }
+                        catch(...)
+                        {
+                            
+                        }
+                    } ,ReturnValue
+                );
+        return ReturnValue;
+    }
+    ThreadID ThreadingState::GetNextID()
+    {
+        std::lock_guard<std::mutex> InternalsLock(m_ThreadInfoMutex);
+        return m_CurrentID;
+    }
+    ThreadID ThreadingState::CurrentID()
+    {
+        std::lock_guard<std::mutex> InternalsLock(m_ThreadInfoMutex);
+        return m_CurrentThread;
+    }
+    void ThreadingState::Sleep(ThreadID ID,float Duration)
+    {
+        std::lock_guard<std::mutex> InternalLock(m_ThreadInfoMutex);
+        auto ThreadIt = m_ActiveThreads.find(ID);
+        if(ThreadIt == m_ActiveThreads.end())
+        {
+            throw std::runtime_error("Invalid thread ID when calling sleep");
+        }
+        std::lock_guard<std::mutex> ThreadLock(ThreadIt->second->WaitMutex);
+        ThreadIt->second->SleepDuration = Duration;
+    }
+    void ThreadingState::Join(ThreadID ID)
+    {
+           
+    }
+};
---
+++ Threading.h
@@ -0,0 +1,96 @@
+#pragma once
+#include "Value.h"
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <unordered_map>
+
+namespace MBLisp
+{
+    class Lock
+    {
+        std::recursive_mutex m_LockMutex;
+        std::condition_variable_any m_LockConditional;
+        std::atomic<bool> m_WasNotified = false;
+    public:
+        Value Wait()
+        {
+            while(true)
+            {
+                std::unique_lock<std::recursive_mutex> CurrentLock(m_LockMutex);
+                m_LockConditional.wait(CurrentLock);
+                if(m_WasNotified)
+                {
+                    m_WasNotified = false;   
+                    break;
+                }
+            }
+            return Value();
+        }
+        Value Get()
+        {
+            m_LockMutex.lock();
+            return Value();
+        }
+        Value Release()
+        {
+            m_LockMutex.unlock();
+            return Value();
+        }
+        Value Notify()
+        {
+            m_WasNotified.store(true);
+            m_LockConditional.notify_one();
+            return Value();
+        }
+    };
+    class ThreadingState
+    {
+        struct ThreadSchedulingInfo
+        {
+            bool Paused = false;
+            float SleepDuration = 0;
+            //not existing means that the scheduler represents the main thread
+            std::unique_ptr<std::thread> SystemThread;
+            bool WakedUp = false;
+            std::mutex WaitMutex;
+            std::condition_variable WaitConditional;
+        };
+        static constexpr float m_SwapTime = 0.005f;
+
+        ThreadID m_CurrentThread = 0;
+        std::atomic<bool> m_Exiting{false};
+        std::atomic<int> m_ThreadCount{0};
+
+        std::atomic<float> m_ElapsedTime{0};
+        std::atomic<clock_t> m_LastClock;
+
+        std::mutex m_ThreadInfoMutex;
+        ThreadID m_CurrentID = 1;
+        std::unordered_map<ThreadID,std::shared_ptr<ThreadSchedulingInfo>> m_ActiveThreads;
+
+        void p_ScheduleNext();
+        void p_StartNext();
+    public:
+        ThreadingState();
+        ~ThreadingState();
+        ThreadingState(ThreadingState const& ) = delete;
+        ThreadingState(ThreadingState&& ) = delete;
+        ThreadingState& operator=(ThreadingState const&) = delete;
+
+        bool MultipleThreadsActive();
+        void WaitForTurn(ThreadID ID);
+
+        void Remove(ThreadID ID);
+        ThreadID AddThread(std::function<void()> Func);
+        ThreadID GetNextID();
+        ThreadID CurrentID();
+
+
+        void Pause(ThreadID ID);
+        void Resume(ThreadID ID);
+        void Sleep(ThreadID ID,float Duration);
+        void Join(ThreadID ID);
+    };
+};
